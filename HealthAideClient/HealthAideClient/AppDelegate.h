aefabe314c7f508bd9dfa723ac574eb3d9f03e7f
zhangcheng3 <zhangcheng3@le.com>
2018-05-21 14:50:26 +0800

    创建工程

---
+++ HealthAideClient/HealthAideClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthAideClient
+//
+//  Created by leeco on 2018/5/21.
+//  Copyright © 2018年 zhangcheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
